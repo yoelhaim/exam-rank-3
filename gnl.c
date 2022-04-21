54ea44ac5a0af82672b5fd7b36a2d7b3daf8f06a
yoelhaim <youssefelhaimer8@gmail.com>
2022-04-21 14:34:43 +0000

    fixed gnl

--- gnl.c
+++ gnl.c
@@ -2,39 +2,43 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-char *ft_dup(char *str)
+char *ft_strdup(char *str)
 {
-    char *s;
     int len = 0;
-    while (str[++len])
-        ;
-    s = malloc(len + 1);
-    if (!s)
-        return NULL;
+    while (str[len])
+        len++;
+    char *new_str = malloc(sizeof(char) * (len + 1));
+    if (!new_str)
+        return (0);
     int i = -1;
-
     while (++i < len)
-    {
-        s[i] = str[i];
-    }
-    s[i] = '\0';
-    return s;
+        new_str[i] = str[i];
+    new_str[i] = 0;
+    return (new_str);
 }
-char *gnl(int fd)
+
+char *get_next_line(int fd)
 {
-    char heap[7000000] = {0}, buff;
-    int i = 0, byte = 0;
-    while ((byte = read(fd, &buff, 1)) && byte > 0)
+
+    char heap[7000000] = {0};
+    char *line = NULL;
+    char buff;
+    int nbyte = 0;
+    int i = 0;
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (0);
+    while ((nbyte = read(fd, &buff, 1)) && nbyte > 0)
     {
         heap[i++] = buff;
         if (buff == '\n')
             break;
     }
-    heap[i] = 0;
-    if (byte < 1 && i == -0)
-        return 0;
-    return (ft_dup(heap));
+    heap[i] = '\0';
+    if (nbyte <= 0 && i == 0)
+        return (0);
+    return ft_strdup(heap);
 }
+
 int main()
 {
     int fd = open("t.txt", O_RDONLY);
